0fb24e50335000a3a351c3260f5a4d6a023d0675
AlfredoCode <gormeet@seznam.cz>
2026-01-24 18:42:18 +0100

    Added project model

---
+++ backend/src/project/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from src.database import Base
+
+class Project(Base):
+    __tablename__ = "Project"
+    
+    ProjectId = Column(Integer, primary_key=True)
+    Title = Column(String(40), nullable=False)
+    Description = Column(String(255), nullable=True)
+    CreatedDate = Column(DateTime, nullable=False)
+    Active = Column(Boolean, nullable=False, default=0)
+
+
+
+    
+    
