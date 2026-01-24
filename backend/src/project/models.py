d33ece3e99ef6329685b7ac83b117644e1218d05
AlfredoCode <gormeet@seznam.cz>
2026-01-24 19:40:50 +0100

    Set nullable for created by employee id to false

--- backend/src/project/models.py
+++ backend/src/project/models.py
@@ -5,7 +5,7 @@ class Project(Base):
     __tablename__ = "Project"
     
     ProjectId = Column(Integer, primary_key=True)
-    Title = Column(String(40), nullable=False)
+    Title = Column(String(40), nullable=False, unique=True)
     Description = Column(String(255), nullable=True)
     CreatedDate = Column(DateTime, nullable=False)
     CreatedByEmployeeId = Column(Integer, ForeignKey('Employee.EmployeeId'), nullable=False)
