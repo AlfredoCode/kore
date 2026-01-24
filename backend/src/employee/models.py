360af9ee0252961d17d63c84f97f36882e89de26
AlfredoCode <gormeet@seznam.cz>
2026-01-24 18:42:28 +0100

    Added Employee models

---
+++ backend/src/employee/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from src.database import Base
+
+class Employee(Base):
+    __tablename__ = "Employee"
+
+    EmployeeId = Column(Integer, primary_key=True)
+    FirstName = Column(String(50), nullable=False)
+    SecondName = Column(String(20), nullable=False)
+    EmployeeRoleType = Column(Integer, ForeignKey('EmployeeRoleType.EmployeeRoleTypeId'), nullable=False)
+    Email = Column(String(40), nullable=False, unique=True)
+    PassHash = Column(String(95), nullable=False)
+    CreatedDate = Column(DateTime, nullable=False)
+    ModifiedDate = Column(DateTime, nullable=True)
+    EmployeeSupervisorId = Column(Integer, ForeignKey('Employee.EmployeeId'), nullable=True)
+    SystemAccess = Column(Boolean, nullable=False, default=0)
+
+class EmployeeRoleType(Base):
+    __tablename__ = "EmployeeRoleType"
+    
+    EmployeeRoleTypeId = Column(Integer, primary_key=True)
+    Description = Column(String(50), nullable=False, unique=True)
+    Active = Column(Boolean, nullable=False, default=0)
+
+class EmployeeAllowedProject(Base):
+    __tablename__ = "EmployeeAllowedProject"
+    
+    EmployeeAllowedProjectId = Column(Integer, primary_key=True)
+    EmployeeId = Column(Integer, ForeignKey('Employee.EmployeeId'), nullable=False)
+    ProjectId = Column(Integer, ForeignKey('Project.ProjectId'), nullable=False)
+    AllowedAccessDate = Column(DateTime, nullable=False)
+    RemovedAccessDate = Column(DateTime, nullable=True)
+
+    
