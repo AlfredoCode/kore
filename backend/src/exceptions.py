8bde8e62b8a4ed77c59ec65ec553e33f333087f4
AlfredoCode <gormeet@seznam.cz>
2026-03-28 10:03:17 +0100

    Fixed proper unauthorized response

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -1,3 +1,6 @@
+from fastapi.responses import JSONResponse
+from src.schemas import GenericResponse
+
 class DatabaseException(Exception):
     def __init__(self, detail: str = "Could not perform database operation"):
         self.detail = detail
@@ -5,3 +8,9 @@ class DatabaseException(Exception):
 class EmailExistsException(Exception):
     def __init__(self, detail: str = "Employee with this email already exists"):
         self.detail = detail
+
+def Unauthorized(msg: str):
+    return JSONResponse(
+        status_code=401,
+        content=GenericResponse(statusCode=401, message=msg).model_dump()
+    )
